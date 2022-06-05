70f76f52757e5049043a052acd47a2b8cdb24be1
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2022-06-06 00:39:13 +0600

    Initialized with tailwind

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+function HomePage() {
+  return (
+    <div className="font-bold text-2xl text-main-color">HomePage</div>
+  )
+}
+
+export default HomePage
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+
+      colors: {
+        "main-color": "#0b0b0b",
+      },
+    },
+  },
+  plugins: [],
+};
